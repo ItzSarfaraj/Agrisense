ebce1bfe01dcea08547d9efed99cb00d882531ee
itzSarfaraj <sarfarajsiddiqui002@gmail.com>
2026-06-20 23:06:49 +0530

    bug fix

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,4 +1,10 @@
 const cloudinary = require("cloudinary").v2;
+console.log("Cloudinary Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("Cloudinary Key:", process.env.CLOUDINARY_API_KEY);
+console.log(
+  "Cloudinary Secret Exists:",
+  !!process.env.CLOUDINARY_API_SECRET
+);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
